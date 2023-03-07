1caabc77632aef2dafb2f5be53bd2d502d6e6544
97103202 <115530988+97103202@users.noreply.github.com>
2023-03-07 11:11:27 +0100

    pizza opdracht af

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>PHP opdracht - vertaler</title>
+    <link rel="stylesheet" href="pizza.css">
+</head>
+
+<body>
+
+    <div id="con">
+        <!-- <div id="pic1"><img src="pizzapic/pic1.png" alt="background pic"> </div> -->
+
+        <!--<div id="pic3"><img src="pizzapic/pic3.png"> </div>-->
+        <div id="pic1">
+            <div id="pic1text">
+                <h1>pizza</h1>
+                <nav>
+                    <ul>
+                        <div class="menu">
+                            <li><a class="order" href="page2.php">menu</a></li>
+                            <li><a class="order" href="page3.php"> order</a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+
+
+</html>
