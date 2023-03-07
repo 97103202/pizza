1c266a1b4e02f70c189355138284a6f930c52d98
97103202 <115530988+97103202@users.noreply.github.com>
2023-03-07 11:22:49 +0100

    pizza opdracht page 2 menu

---
+++ page2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+    <link rel="stylesheet" href="pizza.css">
+</head>
+
+<body>
+    <div id="pic2">
+        <h1 id="pictitle">menu</h1>
+        <br>
+        <br>
+        <h1 class="title">Pizza Margherita</h1>
+        <br>
+        <p class="pic2text">tomatensaus, mozzarella, knoplookolie, oregano</p>
+        <br>
+        <br>
+        <h1 class="title">Pizza Funghi</h1>
+        <br>
+        <p class="pic2text">tomatosaus, mozzarella, champignons, pizzakruiden,</p>
+        <br>
+        <br>
+        <h1 class="title">Pizza Marina</h1>
+        <br>
+        <p class="pic2text">zeevruchten, tonijn, uien</p>
+        <br>
+        <br>
+        <h1 class="title">Pizza Hawai</h1>
+        <br>
+        <p class="pic2text">tomatosaus, mozzarella, ham, ananas, extra mozzarella, knoflookolie</p>
+        <br>
+        <br>
+        <h1 class="title">Pizza Quatro Formaggi</h1>
+        <br>
+        <p class="pic2text">tomattosaus, mozzarella, champignons, parmezaan flakes, peterselie, knoflookolie,
+            truffeltapenade</p>
+
+    </div>
+
+
+
+
+</body>
+
+</html>
